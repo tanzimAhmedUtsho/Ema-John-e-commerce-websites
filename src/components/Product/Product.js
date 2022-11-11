48a9a80e967fe524431c9457c04c9a0566237706
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-11 20:43:42 +0600

    added btn and eventhandler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import "./Product.css";
 
-const Product = (props) => {
-  const { name, img, seller, price, ratings } = props.product;
+const Product = ({ product, handleAddToCart }) => {
+  //   const { product, handleAddToCart } = props;
+  const { name, img, seller, price, ratings } = product;
   return (
     <div className="product">
       <img src={img} alt="" />
@@ -16,8 +19,9 @@ const Product = (props) => {
           <small>Ratings: {ratings} stars </small>
         </p>
       </div>
-      <button>
-        <p>Add to Cart</p>
+      <button onClick={() => handleAddToCart(product)} className="btn-cart">
+        <p className="btn-text">Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
       </button>
     </div>
   );
