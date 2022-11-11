2dbfd938af1ceed2373899a0d73a6a42a1af8398
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-11 12:19:43 +0600

    product info updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,6 +6,19 @@ const Product = (props) => {
   return (
     <div className="product">
       <img src={img} alt="" />
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price: ${price}</p>
+        <p>
+          <small>Seller: {seller}</small>
+        </p>
+        <p>
+          <small>Ratings: {ratings} stars </small>
+        </p>
+      </div>
+      <button>
+        <p>Add to Cart</p>
+      </button>
     </div>
   );
 };
